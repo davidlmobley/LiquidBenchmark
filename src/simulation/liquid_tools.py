5f9e0ae167f1de1a49361395dac8926d8502cda2
davidlmobley <dmobley@gmail.com>
2017-02-03 17:42:15 -0800

    Add draft of new build_mixture_prmtop using SMIRFF systems

--- src/simulation/liquid_tools.py
+++ src/simulation/liquid_tools.py
@@ -23,7 +23,7 @@ def make_path(filename):
 
 class AmberMixtureSystem(object):
     """A pipeline for simulating liquid mixtures using amber parameter files.
-    
+
     Parameters
     ----------
     cas_strings : list(str)
@@ -34,6 +34,7 @@ class AmberMixtureSystem(object):
         Temperature to run simulation.
     """
 
+    # TO DO: MODIFY CONSTRUCTOR AND DATA TO HAVE FFXML FILE FOR SMIRNOFF
     def __init__(self, cas_strings, n_monomers, temperature):
 
         self.cas_strings = cas_strings
@@ -41,40 +42,40 @@ class AmberMixtureSystem(object):
         self.temperature = temperature
 
         identifier = list(itertools.chain(cas_strings, [str(n) for n in n_monomers], [str(temperature).split(' ')[0]]))
-        self.identifier = '_'.join(identifier)        
-        
+        self.identifier = '_'.join(identifier)
+
         self.monomer_pdb_filenames = [DATA_PATH + "monomers/" + string + ".pdb" for string in self.cas_strings]
         self.box_pdb_filename = DATA_PATH + "packmol_boxes/" + self.identifier + ".pdb"
-        
+
         self.inpcrd_filename = DATA_PATH + "tleap/" + self.identifier + ".inpcrd"
         self.prmtop_filename = DATA_PATH + "tleap/" + self.identifier + ".prmtop"
-        
+
         self.equil_dcd_filename = DATA_PATH + "equil/" + self.identifier + "_equil.dcd"
         self.equil_pdb_filename = DATA_PATH + "equil/" + self.identifier + "_equil.pdb"
-        
+
         self.production_dcd_filename = DATA_PATH + "production/" + self.identifier + "_production.dcd"
         self.production_pdb_filename = DATA_PATH + "production/" + self.identifier + "_production.pdb"
         self.production_data_filename = DATA_PATH + "production/" + self.identifier + "_production.csv"
-        
+
         self.gaff_mol2_filenames = [DATA_PATH + "monomers/" + string + ".mol2" for string in self.cas_strings]
         self.frcmod_filenames = [DATA_PATH + "monomers/" + string + ".frcmod" for string in self.cas_strings]
-        
+
         make_path(DATA_PATH + 'monomers/')
         make_path(DATA_PATH + 'packmol_boxes/')
         make_path(DATA_PATH + 'tleap/')
-        
+
         make_path(DATA_PATH + 'equil/')
-        make_path(self.equil_pdb_filename)        
-        
+        make_path(self.equil_pdb_filename)
+
         make_path(DATA_PATH + 'production/')
-        make_path(self.production_dcd_filename)        
+        make_path(self.production_dcd_filename)
 
     @property
     def smiles_strings(self):
         self._smiles_strings = []
         for mlc in self.cas_strings:
             self._smiles_strings.append(openmoltools.cirpy.resolve(mlc, 'smiles'))
-        
+
         return self._smiles_strings
 
     def run(self, just_build=False):
@@ -93,6 +94,68 @@ class AmberMixtureSystem(object):
             if not (os.path.exists(mol2_filename) and os.path.exists(frcmod_filename)):
                 openmoltools.openeye.smiles_to_antechamber(smiles_string, mol2_filename, frcmod_filename)
 
+    def build_mixture_prmtop(gaff_mol2_filenames, box_filename, prmtop_filename, inpcrd_filename, ffxml):
+       """Analog of openmoltools.amber.build_mixture_prmtop which uses SMIRNOFF forcefield (from github.com/open-forcefield-group/smarty) to parameterize small molecules, rather than GAFF.
+
+    Parameters
+    ----------
+    mol2_filenames : list(str)
+        Filenames of GAFF flavored mol2 files.  Each must contain exactly
+        ONE solute.
+    box_filename : str
+        Filename of PDB containing an arbitrary box of the mol2 molecules.
+    prmtop_filename : str
+        output prmtop filename.  Should have suffix .prmtop
+    inpcrd_filename : str
+        output inpcrd filename.  Should have suffix .inpcrd
+    ffxml : str
+        filename containing input SMIRNOFF FFXML file for use in parameterizing the system
+
+
+    Returns
+    -------
+    success : bool
+        True or False as to success of operation
+
+    Notes
+    -----
+    This can be easily broken if there are missing, duplicated, or
+    inconsistent ligand residue names in your box, mol2, and frcmod files.
+    You can use mdtraj to edit the residue names with something like
+    this: trj.top.residue(0).name = "L1"
+"""
+
+        # Read in molecules
+        from smarty import *
+        from openeye.oechem import *
+        import parmed
+        oemols = []
+        for mol2file in gaff_mol2_filenames:
+            mol = oechem.OEGraphMol()
+            ifs = oechem.oemolistream(mol_filename)
+            flavor = oechem.OEIFlavor_Generic_Default | oechem.OEIFlavor_MOL2_Default | oechem.OEIFlavor_MOL2_Forcefield
+            ifs.SetFlavor( oechem.OEFormat_MOL2, flavor)
+            oechem.OEReadMolecule(ifs, mol )
+            oechem.OETriposAtomNames(mol)
+            oemols.append(mol)
+
+
+        # Read in PDB file to get topology
+        pdb = app.PDBFile(box_filename)
+
+        # Load forcefield
+        ff = ForceField(ffxml)
+
+        # Construct system; charging not needed as mol2 files already have charges here
+        system = ff.createSystem( pdb.topology, oemols )
+
+        # Dump to AMBER format
+        structure = parmed.openmm.topsystem.load_topology( pdbfile.topology, system, pdbfile.positions)
+        structure.save(prmtop_filename, overwrite=True)
+        structure.save(inpcrd_filename, format='rst7', overwrite=True)
+
+        return True
+
 
     def build_boxes(self):
         """Build an initial box with packmol and use it to generate AMBER files."""
@@ -101,11 +164,11 @@ class AmberMixtureSystem(object):
             packed_trj.save(self.box_pdb_filename)
 
         if not (os.path.exists(self.inpcrd_filename) and os.path.exists(self.prmtop_filename)):
-            tleap_cmd = openmoltools.amber.build_mixture_prmtop(self.gaff_mol2_filenames, self.frcmod_filenames, self.box_pdb_filename, self.prmtop_filename, self.inpcrd_filename)
+            tleap_cmd = build_mixture_prmtop(self.gaff_mol2_filenames, self.box_pdb_filename, self.prmtop_filename, self.inpcrd_filename, self.ffxml)
 
 
     def equilibrate(self):
-        
+
         if os.path.exists(self.equil_pdb_filename):
             return
 
@@ -121,10 +184,10 @@ class AmberMixtureSystem(object):
 
         state = simulation.context.getState(getEnergy=True)
         print(state.getPotentialEnergy())
-        
+
         print('Minimizing.')
         simulation.minimizeEnergy()
-        
+
         state = simulation.context.getState(getEnergy=True)
         print(state.getPotentialEnergy())
 
@@ -138,7 +201,7 @@ class AmberMixtureSystem(object):
         traj = md.load(self.equil_dcd_filename, top=self.prmtop_filename)[-1]
         traj.save(self.equil_pdb_filename)
 
-    def production(self):  
+    def production(self):
 
         if os.path.exists(self.production_dcd_filename) and os.path.exists(self.production_data_filename):
             return
@@ -152,13 +215,13 @@ class AmberMixtureSystem(object):
         system.addForce(mm.MonteCarloBarostat(PRESSURE, self.temperature, BAROSTAT_FREQUENCY))
 
         simulation = app.Simulation(prmtop.topology, system, integrator)
-        
+
         simulation.context.setPositions(pdb.positions)
         simulation.context.setPeriodicBoxVectors(*pdb.topology.getPeriodicBoxVectors())
         simulation.context.setVelocitiesToTemperature(self.temperature)
-        
+
         print('Production.')
-        
+
         simulation.reporters.append(app.DCDReporter(self.production_dcd_filename, OUTPUT_FREQUENCY))
         simulation.reporters.append(app.StateDataReporter(self.production_data_filename, OUTPUT_DATA_FREQUENCY, step=True, potentialEnergy=True, temperature=True, density=True))
 
